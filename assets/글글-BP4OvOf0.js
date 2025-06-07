c6e7be5c1d68eed30f2574cd66004f219d57f532
sirudev <67226914+chriskim2003@users.noreply.github.com>
2025-06-07 14:11:31 +0900

    Add files via upload

---
+++ assets/글글-BP4OvOf0.js
@@ -0,0 +1,4 @@
+const t="글글",e={ops:[{insert:`글글
+`},{attributes:{header:2},insert:`
+`},{attributes:{bold:!0},insert:"글글"},{attributes:{align:"center",header:2},insert:`
+`}]},n={title:t,content:e};export{e as content,n as default,t as title};
