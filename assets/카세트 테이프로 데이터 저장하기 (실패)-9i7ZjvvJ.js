3570b376fcc07bbe1f8f0c496d7fbb567057458d
sirudev <67226914+chriskim2003@users.noreply.github.com>
2025-06-23 14:55:45 +0900

    Add files via upload

---
+++ assets/카세트 테이프로 데이터 저장하기 (실패)-9i7ZjvvJ.js
@@ -0,0 +1,42 @@
+const t="카세트 테이프로 데이터 저장하기 (실패)",e={ops:[{insert:{video:"https://www.youtube.com/embed/uEIwp7f7PM8"}},{insert:`
+
+자세한 실험 내용은 영상 참조`},{attributes:{align:"center",header:1},insert:`
+`},{attributes:{align:"center"},insert:`
+
+`},{attributes:{bold:!0},insert:"핵심 개념 설명"},{attributes:{align:"center",header:3},insert:`
+`},{attributes:{align:"center"},insert:`
+`},{insert:"아날로그 변조"},{attributes:{align:"center"},insert:`
+`},{insert:"아날로그 신호에 의해 반송파의 파라미터(진폭,주파수,위상)를 변화시켜 전송"},{attributes:{align:"center","code-block":"plain"},insert:`
+`},{insert:"[정보통신기술용어해설 참조]"},{attributes:{align:"center"},insert:`
+
+`},{insert:"디지털 변조"},{attributes:{align:"center"},insert:`
+`},{insert:"디지털 심볼들을, 채널 특성에 맞게끔, 아날로그 파형으로 변환/매핑하는 것"},{attributes:{align:"center","code-block":"plain"},insert:`
+`},{insert:"[정보통신기술용어해설 참조]"},{attributes:{align:"center"},insert:`
+
+`},{insert:"디지털 변조 - 편이변조 (Shift Keying)"},{attributes:{align:"center"},insert:`
+`},{insert:"입력되는 정보 데이터에 따라, 정현 반송파의 진폭,주파수,위상으로 변조/키잉/스위칭"},{attributes:{align:"center","code-block":"plain"},insert:`
+`},{insert:"[정보통신기술용어해설 참조]"},{attributes:{align:"center"},insert:`
+
+`},{insert:"편이변조 - 주파수 편이변조 (FSK)"},{attributes:{align:"center"},insert:`
+`},{insert:"진폭은 일정하나, 여러 이산적인 주파수들에 의해, 반송파를 편이 변조하는 방식"},{attributes:{align:"center","code-block":"plain"},insert:`
+`},{insert:"[정보통신기술용어해설 참조]"},{attributes:{align:"center"},insert:`
+
+
+
+`},{attributes:{bold:!0},insert:"FSK의 장점"},{attributes:{align:"center",header:3},insert:`
+`},{insert:"a. 진폭이 일정하고 잡음에 강함"},{attributes:{align:"center"},insert:`
+`},{insert:"b. 변조/복조가 간단함"},{attributes:{align:"center"},insert:`
+`},{insert:"단점"},{attributes:{align:"center"},insert:`
+`},{insert:"a. 대역폭 효율성이 나쁨"},{attributes:{align:"center"},insert:`
+`},{insert:"b. 더 좋은 방법이 있음 (PSK:근데 동기화가 어려운)"},{attributes:{align:"center"},insert:`
+
+
+`},{attributes:{bold:!0},insert:"결과"},{attributes:{align:"center",header:3},insert:`
+`},{insert:"실험에는 실패했지만 더 나은 성능의 오디오 장비가 있으면 가능할지도 모르겠다고 생각합니다."},{attributes:{align:"center"},insert:`
+`},{insert:"2진 FSK를 쓰면 될 확률이 높겠지만 시간이 너무 길어질 뿐더러"},{attributes:{align:"center"},insert:`
+`},{insert:"지금 가지고 있는 오디오 장비로는 10분정도의 짧은 오디오에도 테이프 늘어짐, 잡음등의 오류가 많이 나옴."},{attributes:{align:"center"},insert:`
+
+
+
+`},{insert:"실험은 멀리 보아야 아름답다"},{attributes:{align:"center",header:2},insert:`
+`}]},n={title:t,content:e};export{e as content,n as default,t as title};
